d8c76f3ee34c564ad72a39c4f55d4b0e23e67032
nursaadat <SNursultan@dar.kz>
2022-12-07 19:32:21 +0100

    add(crud): added field validation on tour creation

--- lib/pages/home/create_route.dart
+++ lib/pages/home/create_route.dart
@@ -14,10 +14,12 @@ class CreateRoute extends StatefulWidget {
 }
 
 class _CreateRouteState extends State<CreateRoute> {
+  final _formKey = GlobalKey<FormState>();
   DateTimeRange? dateRange;
   String name = "";
   String description = "";
   late File image;
+  bool loading = false;
 
   String getFrom() {
     if (dateRange == null) {
@@ -36,7 +38,7 @@ class _CreateRouteState extends State<CreateRoute> {
   }
 
   _getFromGallery() async {
-    PickedFile? pickedFile = await ImagePicker().getImage(
+    XFile? pickedFile = await ImagePicker().pickImage(
       source: ImageSource.gallery,
       maxWidth: 1800,
       maxHeight: 1800,
@@ -49,108 +51,114 @@ class _CreateRouteState extends State<CreateRoute> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          const Padding(
-            padding: EdgeInsets.fromLTRB(12,16,12,4),
-            child: Text('Tour name'),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(12,0,12,8),
-            child: TextField(
-              onChanged: (text) {
-                setState(() {
-                  name = text;
-                });
-              },
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter a tour name',
+      body: Form(
+        key: _formKey,
+        child: Column (
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.fromLTRB(12,16,12,4),
+              child: Text('Tour name'),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12,0,12,8),
+              child: TextFormField(
+                onChanged: (text) {
+                  setState(() => name = text);
+                },
+                validator: (val) => val!.isEmpty ? 'Enter the title' : null,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter a tour name',
+                ),
               ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(12,16,12,4),
-            child: Text('Tour description'),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(12,0,12,8),
-            child: TextField(
-              onChanged: (text) {
-                setState(() {
-                  description = text;
-                });
-              },
-              keyboardType: TextInputType.multiline,
-              maxLines: 4,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter a description',
+            const Padding(
+              padding: EdgeInsets.fromLTRB(12,16,12,4),
+              child: Text('Tour description'),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12,0,12,8),
+              child: TextFormField(
+                onChanged: (text) {
+                  setState(() => description = text);
+                },
+                validator: (val) => val!.isEmpty ? 'Enter the description' : null,
+                keyboardType: TextInputType.multiline,
+                maxLines: 4,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter a description',
+                ),
               ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(12,16,12,4),
-            child: Text('Cover image'),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(12,0,12,8),
-            child: Row(
-              children: [
-                FilledTextButton(
-                    text: 'Add image',
-                    onClicked: () {
-                      _getFromGallery();
-                    }
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(
-                      left: 20
-                  ),
-                  child: Text ('file_name.png'),
-                ),
-              ],
+            const Padding(
+              padding: EdgeInsets.fromLTRB(12,16,12,4),
+              child: Text('Cover image'),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(12,16,12,4),
-            child: Text('Available dates'),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(12,0,12,8),
-            child: Row(
-              children: [
-                Expanded(
-                  child: FilledTextButton(
-                      text: getFrom(),
-                      onClicked: () => pickDateRange(context)
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12,0,12,8),
+              child: Row(
+                children: [
+                  FilledTextButton(
+                      text: 'Add image',
+                      onClicked: () {
+                        _getFromGallery();
+                      }
                   ),
-                ),
-                const SizedBox(width: 8),
-                const Icon(Icons.arrow_forward, color: Colors.blue),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: FilledTextButton(
-                      text: getUntil(),
-                      onClicked: () => pickDateRange(context)
+                  const Padding(
+                    padding: EdgeInsets.only(
+                        left: 20
+                    ),
+                    child: Text ('file_name.png'),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+            const Padding(
+              padding: EdgeInsets.fromLTRB(12,16,12,4),
+              child: Text('Available dates'),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12,0,12,8),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: FilledTextButton(
+                        text: getFrom(),
+                        onClicked: () => pickDateRange(context)
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  const Icon(Icons.arrow_forward, color: Colors.blue),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: FilledTextButton(
+                        text: getUntil(),
+                        onClicked: () => pickDateRange(context)
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
-          CollectionReference ref = FirebaseFirestore.instance.collection('routes');
-          ref.add({
-            "name": name,
-            "description": description,
-          });
+          if (_formKey.currentState!.validate()) {
+            setState(() => loading = true);
+            CollectionReference ref = FirebaseFirestore.instance.collection(
+                'routes');
+            ref.add({
+              "name": name,
+              "description": description,
+            });
+
+          }
           print("Pressed on button!");
         },
+
         label: const Text('Go to the map'),
         backgroundColor: Colors.green,
       ),
