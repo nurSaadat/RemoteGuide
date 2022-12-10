306d8d41bcf2f74ae7111c0bdbdde801435c96e4
nursaadat <SNursultan@dar.kz>
2022-12-10 23:41:28 +0100

    add(googlemap): add place search

--- lib/pages/home/locating.dart
+++ lib/pages/home/locating.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_api_headers/google_api_headers.dart';
+import 'package:google_maps_webservice/places.dart';
 
 class Locating extends StatefulWidget {
   const Locating({Key? key}) : super(key: key);
@@ -11,54 +14,116 @@ class Locating extends StatefulWidget {
   State<Locating> createState() => _Locating();
 }
 
+const kGoogleApiKey = "AIzaSyAZx3e0C2EULlN-xGVRJFBS78JI9esJs04";
+final homeScaffoldKey = GlobalKey<ScaffoldState>();
+
 class _Locating extends State<Locating> {
-  late LatLng currentLatLng = const LatLng(48, 2);
-  final Completer<GoogleMapController> _controller = Completer();
+  // late LatLng currentLatLng = const LatLng(48, 2);
+  // final Completer<GoogleMapController> _controller = Completer();
+  // bool showError = false;
+
+  Set<Marker> markerList = {};
+  final Mode _mode = Mode.fullscreen;
+  late GoogleMapController googleMapController;
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+      appBar: AppBar(
+        title: const Text("Choose locations"),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _determinePosition,
-        label: const Text('Go to current location'),
-        icon: const Icon(Icons.pin_drop),
-      ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              // _controller.complete(controller);
+              googleMapController = controller;
+            },
+            markers: markerList,
+          ),
+          ElevatedButton(onPressed: _handlePressButton, child: const Text("Search places")),
+        ],
+      )
     );
   }
+  //     body: GoogleMap(
+  //       mapType: MapType.normal,
+  //       initialCameraPosition: _kGooglePlex,
+  //       onMapCreated: (GoogleMapController controller) {
+  //         _controller.complete(controller);
+  //       },
+  //     ),
+  //     floatingActionButton: FloatingActionButton.extended(
+  //       onPressed: _determinePosition,
+  //       label: const Text('Go to current location'),
+  //       icon: const Icon(Icons.pin_drop),
+  //     ),
+  //   );
+  // }
+  //
+  // Future<void> _determinePosition() async {
+  //   Position position = await Geolocator.getCurrentPosition();
+  //   setState(() {
+  //     currentLatLng = LatLng(position.latitude, position.longitude);
+  //   });
+  //
+  //   late CameraPosition currentPosition = CameraPosition(
+  //       target: currentLatLng!,
+  //       zoom: 14);
+  //   final GoogleMapController controller = await _controller.future;
+  //   controller.animateCamera(CameraUpdate.newCameraPosition(currentPosition));
+  // }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  Future<void> _handlePressButton() async {
+    Prediction? p = await PlacesAutocomplete.show(
+      context: context,
+      apiKey: kGoogleApiKey,
+      onError: (PlacesAutocompleteResponse response) => print("[ERROOOR] ${response.errorMessage}"),
+      mode: _mode,
+      language: 'en',
+      strictbounds: false,
+      types: [""],
+      decoration: InputDecoration(
+          hintText: 'Search',
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: const BorderSide(color: Colors.white))
+      ),
+      components: [Component(Component.country, "pk")]
+    );
+    print("[INFO]: p in handle press button $p");
+    displayPredictions(p!);
   }
 
-  Future<void> _determinePosition() async {
-    Position position = await Geolocator.getCurrentPosition();
-    setState(() {
-      currentLatLng = LatLng(position.latitude, position.longitude);
-    });
+  Future<void> displayPredictions(Prediction p) async {
+    GoogleMapsPlaces places = GoogleMapsPlaces(
+        apiKey: kGoogleApiKey,
+        apiHeaders: await const GoogleApiHeaders().getHeaders()
+    );
 
-    late CameraPosition _kk = CameraPosition(
-        target: currentLatLng!,
-        zoom: 14);
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kk));
+    print("[INFO]: p in function $p, ${p.placeId}");
+    
+    PlacesDetailsResponse detail = await places.getDetailsByPlaceId(p.placeId!);
+
+    final lat = detail.result.geometry?.location.lat;
+    final lng = detail.result.geometry?.location.lng;
+
+    print("[INFO]: Lat and Lng $lat, $lng");
+
+    markerList.clear();
+    markerList.add(Marker(
+      markerId:  const MarkerId("0"),
+      position: LatLng(lat!, lng!),
+      infoWindow: InfoWindow(title: detail.result.name)
+    ));
+
+    googleMapController.animateCamera(CameraUpdate.newLatLngZoom(LatLng(lat, lng), 14.0));
   }
 }
